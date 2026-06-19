e43c4a7f2a069170ce460a2bb189d7ba017c824d
Jamie Banks <jamie@jdbnet.co.uk>
2026-06-19 19:12:53 +0100

    feat: automate virtual environment setup and use exec for gunicorn process management

--- run.sh
+++ run.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+if [ ! -d "venv" ]; then
+    echo "Creating virtual environment in 'venv'..."
+    python3 -m venv venv
+    source venv/bin/activate
+    if [ -f "requirements.txt" ]; then
+        echo "Installing requirements..."
+        pip install -r requirements.txt
+    fi
+else
+    source venv/bin/activate
+fi
+
 cd frontend && npm run build && cd ..
-gunicorn --bind 0.0.0.0:5000 --workers 1 --worker-class geventwebsocket.gunicorn.workers.GeventWebSocketWorker app:app --log-level info
+exec gunicorn --bind 0.0.0.0:5000 --workers 1 --worker-class geventwebsocket.gunicorn.workers.GeventWebSocketWorker app:app --log-level info
